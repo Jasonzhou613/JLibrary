5cb5be97e44f8352b687ada73f524cdb1b499c11
Jason <767372300@qq.com>
2016-08-22 18:26:20 +0800

    完善Gallery

--- library/src/com/ttsea/jlibrary/photo/gallery/GalleryActivity.java
+++ library/src/com/ttsea/jlibrary/photo/gallery/GalleryActivity.java
@@ -3,12 +3,9 @@ package com.ttsea.jlibrary.photo.gallery;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
-import android.text.Layout;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,10 +23,8 @@ import com.ttsea.jlibrary.base.BaseActivity;
 import com.ttsea.jlibrary.common.ImageLoader;
 import com.ttsea.jlibrary.common.JLog;
 import com.ttsea.jlibrary.photo.select.ImageItem;
-import com.ttsea.jlibrary.utils.BitmapUtils;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -56,7 +51,6 @@ public class GalleryActivity extends BaseActivity implements OnClickListener,
     private Button btnDelete;
 
     private ViewPagerFixed viwePager;
-    private ArrayList<View> photoViews;
     private MyPageAdapter adapter;
 
     /** 获取前一个activity传过来的图片list */
@@ -109,8 +103,7 @@ public class GalleryActivity extends BaseActivity implements OnClickListener,
         btnSavePic.setVisibility(canSave ? View.VISIBLE : View.INVISIBLE);
         btnDelete.setVisibility(canDel ? View.VISIBLE : View.INVISIBLE);
 
-        initPhotoViews();
-        adapter = new MyPageAdapter(photoViews);
+        adapter = new MyPageAdapter(selectedList);
         viwePager.setAdapter(adapter);
         viwePager.setPageMargin(10);
         viwePager.setCurrentItem(currentPosition);
@@ -118,16 +111,12 @@ public class GalleryActivity extends BaseActivity implements OnClickListener,
         refreshTvIndex();
     }
 
-    private void initPhotoViews() {
-        refreshPhotoViews();
-    }
-
     private void setLoadingViewVisibility(View childView, int visibility) {
         View parent;
-        ViewParent viewParent = childView.getParent();
-        if (viewParent == null) {
+        if (childView == null) {
             return;
         }
+        ViewParent viewParent = childView.getParent();
         if (viewParent instanceof FrameLayout) {
             parent = (FrameLayout) viewParent;
         } else {
@@ -140,13 +129,43 @@ public class GalleryActivity extends BaseActivity implements OnClickListener,
         }
     }
 
-    private void refreshPhotoViews() {
-        LayoutInflater inflater = LayoutInflater.from(mActivity);
-        if (photoViews == null) {
-            photoViews = new ArrayList<View>();
-        }
-        photoViews.clear();
+    private void onOkBtnClicked() {
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(GalleryConstants.KEY_SELECTED_LIST, (Serializable) selectedList);
+        intent.putExtras(bundle);
+        setResult(Activity.RESULT_OK, intent);
 
+        finish();
+    }
+
+    private void onDeleteBtnClicked() {
+        selectedList.remove(currentPosition);
+
+        if (currentPosition >= selectedList.size() && selectedList.size() != 0) {
+            currentPosition = selectedList.size() - 1;
+        } else if (currentPosition != 0) {
+            currentPosition--;
+        }
+        JLog.d(TAG, "removed currentPosition:" + currentPosition);
+        adapter.notifyDataSetChanged();
+        refreshTvIndex();
+
+        if (selectedList.size() == 0) {
+            onOkBtnClicked();
+        }
+    }
+
+    public void refreshTvIndex() {
+        if (selectedList.size() <= 1) {
+            tvIndex.setVisibility(View.INVISIBLE);
+        } else {
+            tvIndex.setVisibility(View.VISIBLE);
+        }
+        tvIndex.setText((currentPosition + 1) + "/" + selectedList.size());
+    }
+
+    private void displayImage(ImageItem item, ImageView imageView) {
         ImageLoader.ImageLoadingListener listener = new ImageLoader.ImageLoadingListener() {
             @Override
             public void onLoadingStarted(String s, View view) {
@@ -169,62 +188,13 @@ public class GalleryActivity extends BaseActivity implements OnClickListener,
             }
         };
 
+        JLog.d(TAG, "childrenCount:" + viwePager.getChildCount());
 
-        for (int i = 0; i < selectedList.size(); i++) {
-            View itemView = inflater.inflate(R.layout.photo_gallery_item, null);
-
-            PhotoView pvImage = (PhotoView) itemView.findViewById(R.id.pvImage);
-            pvImage.setBackgroundColor(0xff000000);
-
-            ImageItem item = selectedList.get(i);
-
-            if (item.isNetWorkImage()) {
-                ImageLoader.getInstance().displayImageForGallery(mActivity, item.getPath(), pvImage, listener);
-            } else {
-                ImageLoader.getInstance().displayImageForGallery(mActivity, "file://" + item.getPath(), pvImage, listener);
-            }
-            itemView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-
-            photoViews.add(itemView);
-        }
-    }
-
-    private void onOkBtnClicked() {
-        Intent intent = new Intent();
-        Bundle bundle = new Bundle();
-        bundle.putSerializable(GalleryConstants.KEY_SELECTED_LIST, (Serializable) selectedList);
-        intent.putExtras(bundle);
-        setResult(Activity.RESULT_OK, intent);
-
-        finish();
-    }
-
-    private void onDeleteBtnClicked() {
-        int removePosition = currentPosition;
-        selectedList.remove(currentPosition);
-        photoViews.remove(currentPosition);
-
-        if (currentPosition >= photoViews.size() && photoViews.size() != 0) {
-            currentPosition = photoViews.size() - 1;
-        } else if (currentPosition != 0) {
-            currentPosition--;
-        }
-        JLog.d(TAG, "removed position:" + removePosition + ", currentPosition:" + currentPosition);
-        adapter.notifyDataSetChanged();
-
-        if (photoViews.size() == 0) {
-            onOkBtnClicked();
-        }
-        refreshTvIndex();
-    }
-
-    public void refreshTvIndex() {
-        if (selectedList.size() <= 1) {
-            tvIndex.setVisibility(View.INVISIBLE);
+        if (item.isNetWorkImage()) {
+            ImageLoader.getInstance().displayImageForGallery(mActivity, item.getPath(), imageView, listener);
         } else {
-            tvIndex.setVisibility(View.VISIBLE);
+            ImageLoader.getInstance().displayImageForGallery(mActivity, "file://" + item.getPath(), imageView, listener);
         }
-        tvIndex.setText((currentPosition + 1) + "/" + selectedList.size());
     }
 
     /** 监听返回按钮 */
@@ -249,7 +219,6 @@ public class GalleryActivity extends BaseActivity implements OnClickListener,
         }
     }
 
-
     @Override
     public void onPageScrollStateChanged(int arg0) {
 
@@ -267,34 +236,46 @@ public class GalleryActivity extends BaseActivity implements OnClickListener,
     }
 
     private class MyPageAdapter extends PagerAdapter {
-        private ArrayList<View> listViews;
+        private List<ImageItem> items;
 
-        public MyPageAdapter(ArrayList<View> listViews) {
-            this.listViews = listViews;
+        public MyPageAdapter(List<ImageItem> listViews) {
+            this.items = listViews;
         }
 
         public int getCount() {
-            return listViews.size();
+            return items.size();
         }
 
         public void destroyItem(View container, int position, Object object) {
-            JLog.d(TAG, "destroyItem, position:" + (position % getCount()));
-
-            try {
-                ((ViewPagerFixed) container).removeView(listViews.get(position % getCount()));
-            } catch (Exception e) {
-                JLog.e(TAG, "Exception e:" + e.toString());
+            JLog.d(TAG, "destroyItem, position:" + position);
+            if (container instanceof ViewPagerFixed && object instanceof View) {
+                ((ViewPagerFixed) container).removeView(((View) object));
             }
         }
 
         public Object instantiateItem(ViewGroup container, int position) {
-            try {
-                container.addView(listViews.get(position % getCount()), 0);
-            } catch (Exception e) {
-                JLog.e(TAG, "Exception e:" + e.toString());
-            }
-            JLog.d(TAG, "instantiateItem, position:" + (position % getCount()));
-            return listViews.get(position % getCount());
+            JLog.d(TAG, "instantiateItem, position:" + position);
+
+            LayoutInflater inflater = LayoutInflater.from(mActivity);
+
+            View itemView = inflater.inflate(R.layout.photo_gallery_item, null);
+            PhotoView pvImage = (PhotoView) itemView.findViewById(R.id.pvImage);
+            pvImage.setBackgroundColor(0xff000000);
+
+            ImageItem item = selectedList.get(position);
+
+            itemView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+            itemView.setTag(item.getPath());
+            displayImage(item, pvImage);
+
+            container.addView(itemView, 0);
+
+            return itemView;
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
         }
 
         public boolean isViewFromObject(View view, Object object) {
